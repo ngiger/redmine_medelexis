0ba0d8d8de32d24060ce19adc33ba98dd207ce6d
Niklaus Giger <niklaus.giger@member.fsf.org>
2021-03-19 21:34:20 +0100

    [22178] Do not action in hook_listener, to avoid stack overflow

--- lib/medelexis_hook_listener.rb
+++ lib/medelexis_hook_listener.rb
@@ -11,18 +11,15 @@ class MedelexisHookListener < Redmine::Hook::ViewListener
   end
 
   def view_users_form(context={})
-    RedmineMedelexis.log_to_system("logger redmine:#{view_users_form} User.current.admin '#{User.current.admin.inspect}'")
     return nil unless User.current.admin
     userId = context[:user].id
     apiKey = User.find(userId).api_key
-    RedmineMedelexis.log_to_system("logger redmine:#{view_users_form} userId '#{userId.inspect}' apiKey '#{apiKey.inspect}'")
     return content_tag(:a, l("gen_license_jar"), :href => "/my/#{context[:user].login}/license") + content_tag("div", "#{l(:setting_mail_handler_api_key)} #{apiKey}")
   end
 
   def log_issue(action, context)
     issue = context[:issue]
     params = context[:params]
-    RedmineMedelexis.log_to_system("logger redmine:#{action} issue '#{issue.id}'")
   end
 
   # :controller_issues_edit_after_save    :params, :issue, :time_entry, :journal
